99ce8edad561a20f179cde3947d48d636e913c18
Pryamilov_VV <v.pryamilov@tanais.ru>
2025-04-04 16:57:19 +0300

    feat: добавление структур и методов для формирования логов.

--- Tanais.StructuredLogging/Tanais.StructuredLogging.Shared/ModuleConstants.cs
+++ Tanais.StructuredLogging/Tanais.StructuredLogging.Shared/ModuleConstants.cs
@@ -5,6 +5,27 @@ namespace Tanais.StructuredLogging.Constants
 {
   public static class Module
   {
-
+    
+    /// <summary>
+    /// Логирование.
+    /// </summary>
+    public static class Logging
+    {
+      /// <summary>
+      /// Значения для записи при отсутствии данных.
+      /// </summary>
+      public static class DefaultValuesConstLog
+      {
+        [Public]
+        public const string Type = "Id";
+        [Public]
+        public const string Value = "Not defined";
+        [Public]
+        public const string MessageText = "Empty";
+        [Public]
+        public const string StackTrace = "Empty";
+      }
+      
+    }
   }
 }
